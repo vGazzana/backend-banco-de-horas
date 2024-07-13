3692d085d7b2ffc73321ed0209301cace5faa25b
Vinicius Gazzana <vini.gazzana@gmail.com>
2024-07-13 11:38:55 -0300

    FEAT - SignIn Route

---
+++ src/utils/populateDB.js
@@ -0,0 +1,38 @@
+import { PrismaClient } from "@prisma/client";
+import { generateHash } from "./crypto.js";
+
+const prisma = new PrismaClient();
+
+// Função para popular o banco de dados
+const populateDatabase = async () => {
+  try {
+    // Criar dois usuários com senhas hashadas
+    await prisma.users.createMany({
+      data: [
+        {
+          email: `adm@adm.com`,
+          password: generateHash("admin"),
+          type: "ADMINISTRATOR",
+        },
+        {
+          email: `normal@normal.com`,
+          password: generateHash("normal"),
+          type: "NORMAL",
+        },
+      ],
+    });
+
+    console.log("Usuários criados com sucesso!");
+  } catch (error) {
+    console.error("Erro ao popular o banco de dados:", error);
+  }
+};
+
+// Execute a função para popular o banco de dados ao chamar o script
+populateDatabase()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
